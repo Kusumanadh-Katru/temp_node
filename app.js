708c0b4155f6a72e815a0daf3f9bebf8510e9724
Admin <admin@Admins-MacBook-Pro.local>
2024-07-21 17:55:04 +0530

    Removed node moduels

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-const _ = require('loadash');
-
-const items = [1 ,[2,[3,[4]]]]
-
-const newItems = _.flattendDeep(items)
-console.log(newItems)
+console.log("it is working")
+console.log('Yess, this is the change i did')
