f1463b1c9ab524b739f99793a9eeb2a699311857
Admin <admin@Admins-MacBook-Pro.local>
2024-08-04 17:34:56 +0530

    Fundementalas are done

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-console.log("it is working")
-console.log('Yess, this is the change i did')
+const http = require('http')
+const fs = require ('fs')
+
+
+http
+.createServer((req,res)=>{
+
+    const fileStream = fs.createReadStream('./content/first.txt','utf8');
+
+    fileStream.on('open',()=>{
+     fileStream.pipe(res)
+    })
+    fileStream.on('error',(err)=>{
+        res.end(err)
+    })
+})
+
+.listen(3000)
